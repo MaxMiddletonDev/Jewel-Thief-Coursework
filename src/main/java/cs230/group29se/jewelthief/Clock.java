091b8d8cefce3f8c5eb8146100faa6a543dca0cf
CSH <jobby2812@gmail.com>
2025-11-17 21:13:42 +0000

    add clock and Gate

---
+++ src/main/java/cs230/group29se/jewelthief/Clock.java
@@ -0,0 +1,31 @@
+package cs230.group29se.jewelthief;
+
+/**
+ * Allows for a clock to be created which change the time left in a level.
+ * @author Charlie
+ * @version 0.1
+ */
+public class Clock extends Destroyable {
+
+    /**
+     * The amount time will change by when picked up by a player,
+     * floor thief, or smart thief.
+     */
+    private static final int TIME_CHANGE = 5;
+
+    /**
+     * Allows for a clock to be created at a position in the level.
+     * @param position position the clock will have.
+     */
+    public Clock(final int[] position) {
+        super(position);
+    }
+
+    /**
+     * Changes the time left in the level.
+     * Needs timer to be implemented before it can be created.
+     */
+    public void interact() {
+
+    }
+}
---
+++ src/main/java/cs230/group29se/jewelthief/Gate.java
@@ -0,0 +1,38 @@
+package cs230.group29se.jewelthief;
+
+/**
+ * An obstacle that blocks the player that can be opened.
+ * Will implement Remove when remove() can be made.
+ * @author Charlie
+ * @version 1.0
+ */
+public class Gate {
+    /**
+     * The colour the gate is
+     */
+    private Colour colour;
+
+    /**
+     *  It is not in the design but gate should
+     *  have a position? i cant think what it would do maybe data
+     *  preservation but would that data not be gotten by the tile itself
+     *  {@code private int[] position;}
+     */
+
+    /**
+     * Allows for gates to be made in the l
+     * @param colour The colour of the gate
+     */
+    public Gate(Colour colour) {
+        this.colour = colour;
+    }
+
+    /**
+     * Gets the colour of the gate.
+     * @return the colour of the gate.
+     */
+    public Colour getColour() {
+        return colour;
+    }
+
+}
