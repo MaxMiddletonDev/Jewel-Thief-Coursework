25bde9caebc2de43a1bf2d8065104243dd7d22db
CSH <jobby2812@gmail.com>
2025-11-22 17:07:57 +0000

    Create Colour.java

---
+++ src/main/java/cs230/group29se/jewelthief/Colour.java
@@ -0,0 +1,13 @@
+package cs230.group29se.jewelthief;
+
+/**
+ * An enum of the different possible colours for doors, gates and tiles.
+ * @author Charlie
+ * @version 1.0
+ */
+public enum Colour {
+    RED,
+    YELLOW,
+    GREEN,
+    BLUE;
+}
