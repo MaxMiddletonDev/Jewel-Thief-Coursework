e3ef07a873edfbd90553f8253d70bd63688a4e5d
3trn <maxedwardmiddleton@gmail.com>
2025-12-01 21:28:59 +0000

    Items now get collected

--- src/main/java/cs230/group29se/jewelthief/Player.java
+++ src/main/java/cs230/group29se/jewelthief/Player.java
@@ -115,7 +115,18 @@ public class Player implements MoveableCharacter {
         while (x > 0 && x <= level.getWidth() && y > 0 && y <= level.getHeight()) {
             Tile target = level.getTile(x - 1, y - 1);
             if (target != null && currentTile.isValidMove(target)) {
+                Object occupant = target.getOccupying();
+                if (occupant instanceof Gate) {
+                    return;
+                }
                 this.currentTile = target;
+
+                if (occupant instanceof Item item) {
+                    collectItem(item);
+                    target.setOccupying(null);
+                    level.removeItem(item);
+                }
+
                 return;
             }
             x += dx;
