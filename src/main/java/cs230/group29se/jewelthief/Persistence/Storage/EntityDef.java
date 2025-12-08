59baebff8154f6ba2c038f82c780410e579fea27
Gustas Rove <78168385+TheMixas@users.noreply.github.com>
2025-12-08 10:05:04 +0000

    FINAL SUBMISSION
    COMMENTS ON ENTITY DEF AND LEVEL DEF. THE FINAL SUB

--- src/main/java/cs230/group29se/jewelthief/Persistence/Storage/EntityDef.java
+++ src/main/java/cs230/group29se/jewelthief/Persistence/Storage/EntityDef.java
@@ -1,7 +1,7 @@
 package cs230.group29se.jewelthief.Persistence.Storage;
 
 /**
- * One starting entity defined in a level file.
+ * Represents one starting entity defined in a level file.
  * <p>
  * Examples:
  * <ul>
@@ -14,17 +14,31 @@ package cs230.group29se.jewelthief.Persistence.Storage;
  *   <li>LOOT 2 3 DOLLAR
  *       → type = "LOOT", x = 2, y = 3, arg1 = "DOLLAR"</li>
  * </ul>
+ * </p>
+ *
  * @author Iyaad
  * @version 1.0
  */
 public class EntityDef {
+    // The type of the entity (e.g., PLAYER, FLYING, FOLLOWER, LOOT)
     private String type;
-    private int x;
-    private int y;
-    private String arg1;
-    private String arg2;
+    private int x;       // The x-coordinate of the entity
+    private int y;       // The y-coordinate of the entity
+    private String arg1; // Optional argument 1 for the entity
+    private String arg2; // Optional argument 2 for the entity
 
-    public EntityDef(String type, int x, int y, String arg1, String arg2) {
+    /**
+     * Constructs an EntityDef object with the specified parameters.
+     *
+     * @param type The type of the entity
+     * @param x The x-coordinate of the entity
+     * @param y The y-coordinate of the entity
+     * @param arg1 Optional argument 1 for the entity
+     * @param arg2 Optional argument 2 for the entity
+     */
+    public EntityDef(final String type, final int x,
+                     final int y, final String arg1,
+                     final String arg2) {
         this.setType(type);
         this.setX(x);
         this.setY(y);
@@ -32,43 +46,93 @@ public class EntityDef {
         this.setArg2(arg2);
     }
 
+    /**
+     * Gets the type of the entity.
+     *
+     * @return The type of the entity
+     */
     public String getType() {
         return type;
     }
 
-    public void setType(String type) {
+    /**
+     * Sets the type of the entity.
+     *
+     * @param type The type to set
+     */
+    public void setType(final String type) {
         this.type = type;
     }
 
+    /**
+     * Gets the x-coordinate of the entity.
+     *
+     * @return The x-coordinate of the entity
+     */
     public int getX() {
         return x;
     }
 
-    public void setX(int x) {
+    /**
+     * Sets the x-coordinate of the entity.
+     *
+     * @param x The x-coordinate to set
+     */
+    public void setX(final int x) {
         this.x = x;
     }
 
+    /**
+     * Gets the y-coordinate of the entity.
+     *
+     * @return The y-coordinate of the entity
+     */
     public int getY() {
         return y;
     }
 
-    public void setY(int y) {
+    /**
+     * Sets the y-coordinate of the entity.
+     *
+     * @param y The y-coordinate to set
+     */
+    public void setY(final int y) {
         this.y = y;
     }
 
+    /**
+     * Gets the optional argument 1 for the entity.
+     *
+     * @return The optional argument 1
+     */
     public String getArg1() {
         return arg1;
     }
 
-    public void setArg1(String arg1) {
+    /**
+     * Sets the optional argument 1 for the entity.
+     *
+     * @param arg1 The optional argument 1 to set
+     */
+    public void setArg1(final String arg1) {
         this.arg1 = arg1;
     }
 
+    /**
+     * Gets the optional argument 2 for the entity.
+     *
+     * @return The optional argument 2
+     */
     public String getArg2() {
         return arg2;
     }
 
-    public void setArg2(String arg2) {
+    /**
+     * Sets the optional argument 2 for the entity.
+     *
+     * @param arg2 The optional argument 2 to set
+     */
+    public void setArg2(final String arg2) {
         this.arg2 = arg2;
     }
 }
--- src/main/java/cs230/group29se/jewelthief/Persistence/Storage/LevelDef.java
+++ src/main/java/cs230/group29se/jewelthief/Persistence/Storage/LevelDef.java
@@ -3,108 +3,230 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Class that shows properties for each level (static definition from levelX.txt).
+ * Represents the static definition of a level,
+ * including its properties and entities,
+ * as defined in a level file (e.g., levelX.txt).
+ * <p>
+ * This class contains information about the
+ * level's dimensions, tiles, entities,
+ * time limit, and other relevant properties.
+ * </p>
+ *
  * @author Iyaad
  * @version 1.0
  */
 public class LevelDef {
-    private String levelId;                 // e.g. "1" for level8.txt
-    private List<String> tiles;            // tile rows
-    private ArrayList<EntityDef> entities; // all entities (PLAYER, FLYING, LOOT, GATE, etc.)
-    private int width;                     // SIZE w h -> w
-    private int height;                    // SIZE w h -> h
-    private List<EntityDef> npcStartStates; // FLYING, FOLLOWER, SMART, ...
-    private List<EntityDef> playerStartState; // optional list if you want, usually size 0 or 1
-    private int timeLimitSec;              // TIME value
-    private List<EntityDef> gates;         // GATE entities
-    private List<EntityDef> items;         // LOOT, BOMB, LEVER, ...
-    private EntityDef playerStart;         // for convenience; direct reference to the PLAYER entity
+    // The logical ID of the level (e.g., "1" for level8.txt)
+    private String levelId;
+    private List<String> tiles;
+    private ArrayList<EntityDef> entities; // All entities in the level
+    private int width;
+    private int height;
+    private List<EntityDef> npcStartStates;
+    // Optional list for player start states, usually size 0 or 1
+    private List<EntityDef> playerStartState;
+    private int timeLimitSec;
+    private List<EntityDef> gates;
+    private List<EntityDef> items;
+    private EntityDef playerStart;
 
+    /**
+     * Gets the logical ID of the level.
+     *
+     * @return The level ID.
+     */
     public String getLevelId() {
         return levelId;
     }
 
-    public void setLevelId(String levelId) {
+    /**
+     * Sets the logical ID of the level.
+     *
+     * @param levelId The level ID to set.
+     */
+    public void setLevelId(final String levelId) {
         this.levelId = levelId;
     }
 
+    /**
+     * Gets the tile rows representing the level layout.
+     *
+     * @return A list of tile rows.
+     */
     public List<String> getTiles() {
         return tiles;
     }
 
-    public void setTiles(List<String> tiles) {
+    /**
+     * Sets the tile rows representing the level layout.
+     *
+     * @param tiles The list of tile rows to set.
+     */
+    public void setTiles(final List<String> tiles) {
         this.tiles = tiles;
     }
 
+    /**
+     * Gets all entities in the level.
+     *
+     * @return A list of entities.
+     */
     public ArrayList<EntityDef> getEntities() {
         return entities;
     }
 
-    public void setEntities(ArrayList<EntityDef> entities) {
+    /**
+     * Sets all entities in the level.
+     *
+     * @param entities The list of entities to set.
+     */
+    public void setEntities(final ArrayList<EntityDef> entities) {
         this.entities = entities;
     }
 
+    /**
+     * Gets the width of the level.
+     *
+     * @return The width of the level.
+     */
     public int getWidth() {
         return width;
     }
 
-    public void setWidth(int width) {
+    /**
+     * Sets the width of the level.
+     *
+     * @param width The width to set.
+     */
+    public void setWidth(final int width) {
         this.width = width;
     }
 
+    /**
+     * Gets the height of the level.
+     *
+     * @return The height of the level.
+     */
     public int getHeight() {
         return height;
     }
 
-    public void setHeight(int height) {
+    /**
+     * Sets the height of the level.
+     *
+     * @param height The height to set.
+     */
+    public void setHeight(final int height) {
         this.height = height;
     }
 
+    /**
+     * Gets the starting states of NPCs in the level.
+     *
+     * @return A list of NPC starting states.
+     */
     public List<EntityDef> getNpcStartStates() {
         return npcStartStates;
     }
 
-    public void setNpcStartStates(List<EntityDef> npcStartStates) {
+    /**
+     * Sets the starting states of NPCs in the level.
+     *
+     * @param npcStartStates The list of NPC starting states to set.
+     */
+    public void setNpcStartStates(
+            final List<EntityDef> npcStartStates) {
         this.npcStartStates = npcStartStates;
     }
 
+    /**
+     * Gets the optional list of player start states.
+     *
+     * @return A list of player start states.
+     */
     public List<EntityDef> getPlayerStartState() {
         return playerStartState;
     }
 
-    public void setPlayerStartState(List<EntityDef> playerStartState) {
+    /**
+     * Sets the optional list of player start states.
+     *
+     * @param playerStartState The list of player start states to set.
+     */
+    public void setPlayerStartState(
+            final List<EntityDef> playerStartState) {
         this.playerStartState = playerStartState;
     }
 
+    /**
+     * Gets the time limit for the level in seconds.
+     *
+     * @return The time limit in seconds.
+     */
     public int getTimeLimitSec() {
         return timeLimitSec;
     }
 
-    public void setTimeLimitSec(int timeLimitSec) {
+    /**
+     * Sets the time limit for the level in seconds.
+     *
+     * @param timeLimitSec The time limit to set.
+     */
+    public void setTimeLimitSec(final int timeLimitSec) {
         this.timeLimitSec = timeLimitSec;
     }
 
+    /**
+     * Gets the GATE entities in the level.
+     *
+     * @return A list of GATE entities.
+     */
     public List<EntityDef> getGates() {
         return gates;
     }
 
-    public void setGates(List<EntityDef> gates) {
+    /**
+     * Sets the GATE entities in the level.
+     *
+     * @param gates The list of GATE entities to set.
+     */
+    public void setGates(final List<EntityDef> gates) {
         this.gates = gates;
     }
 
+    /**
+     * Gets the items in the level.
+     *
+     * @return A list of items.
+     */
     public List<EntityDef> getItems() {
         return items;
     }
 
-    public void setItems(List<EntityDef> items) {
+    /**
+     * Sets the items in the level.
+     *
+     * @param items The list of items to set.
+     */
+    public void setItems(final List<EntityDef> items) {
         this.items = items;
     }
 
+    /**
+     * Gets the direct reference to the PLAYER entity.
+     *
+     * @return The PLAYER entity.
+     */
     public EntityDef getPlayerStart() {
         return playerStart;
     }
 
-    public void setPlayerStart(EntityDef playerStart) {
+    /**
+     * Sets the direct reference to the PLAYER entity.
+     *
+     * @param playerStart The PLAYER entity to set.
+     */
+    public void setPlayerStart(final EntityDef playerStart) {
         this.playerStart = playerStart;
     }
 }
