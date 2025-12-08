8a479dc4f856222af06fe7511db95cb58f776dfe
3trn <maxedwardmiddleton@gmail.com>
2025-12-08 06:20:57 +0000

    JAVADOC for SOUND MANAGER

--- src/main/java/cs230/group29se/jewelthief/Game/SoundManager.java
+++ src/main/java/cs230/group29se/jewelthief/Game/SoundManager.java
@@ -3,10 +3,17 @@ package cs230.group29se.jewelthief.Game;
 import javafx.scene.media.AudioClip;
 import java.net.URL;
 
+/**
+ * Sound Manager to handle all the Sound Effects and Music within the Game
+ * This allows for sound to be played in specific points where needed.
+ *
+ * @author Max Middleton
+ */
 public class SoundManager {
     private static final double VOLUME = 0.1;
     private static final String BASE_PATH = "/cs230/group29se/jewelthief/Audios/";
 
+    // End File path for Audios
     public static final AudioClip COLLECT = load("Collect.mp3");
     public static final AudioClip BOMB = load("Bomb.mp3");
     public static final AudioClip DEATH = load("Death.mp3");
@@ -16,6 +23,12 @@ public class SoundManager {
     public static final AudioClip START = load("Start.mp3");
     public static final AudioClip WIN = load("Win.mp3");
 
+    /**
+     * Load Method Concatenates the base path and filename.
+     * Has a catch for incorrect files
+     * @param filename Audio Files to be added
+     * @return null;
+     */
     private static AudioClip load(String filename) {
         try {
             URL resource = SoundManager.class.getResource(BASE_PATH + filename);
@@ -30,24 +43,36 @@ public class SoundManager {
         }
     }
 
+    /**
+     * Plays a sound that represents Collect.
+     */
     public static void playCollect() {
         if (COLLECT != null) {
             COLLECT.play();
         }
     }
 
+    /**
+     * Plays a sound that represents Bomb.
+     */
     public static void playBomb() {
         if (BOMB != null) {
             BOMB.play();
         }
     }
 
+    /**
+     * Plays a sound that represents Death.
+     */
     public static void playDeath() {
         if (DEATH != null) {
             DEATH.play();
         }
     }
 
+    /**
+     * Plays a sound that represents Game.
+     */
     public static void playGame() {
         if (GAME != null) {
             GAME.stop();
@@ -56,6 +81,9 @@ public class SoundManager {
         }
     }
 
+    /**
+     * Plays a sound that represents Main Menu.
+     */
     public static void playMainMenu() {
         if (MAINMENU != null) {
             MAINMENU.stop();
@@ -63,16 +91,28 @@ public class SoundManager {
             MAINMENU.play(VOLUME);
         }
     }
+
+    /**
+     * Plays a sound that represents Pause.
+     */
     public static void playPause() {
         if (PAUSE != null) {
             PAUSE.play();
         }
     }
+
+    /**
+     * Plays a sound that represents Start.
+     */
     public static void playStart() {
         if (START != null) {
             START.play(VOLUME);
         }
     }
+
+    /**
+     * Plays a sound that represents Win.
+     */
     public static void playWin() {
         if (WIN != null) {
             WIN.play();
