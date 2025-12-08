e3a3865261c4a64f769db720f9f9414e6f056fc4
3trn <maxedwardmiddleton@gmail.com>
2025-12-08 03:07:55 +0000

    Introduce new designs to items and new skins

--- src/main/java/cs230/group29se/jewelthief/Cosmetics/SkinRegistry.java
+++ src/main/java/cs230/group29se/jewelthief/Cosmetics/SkinRegistry.java
@@ -9,8 +9,8 @@ public class SkinRegistry {
 
     static {
         register(new Skin(SkinId.DEFAULT_GUY, "Default Guy", "/cs230/group29se/jewelthief/Images/Entities/Player/PLAYER.png"));
-        register(new Skin(SkinId.SPEED_GUY, "Speed guy", "/cs230/group29se/jewelthief/Images/Entities/Player/PLAYER_SKIN_1.png"));
-        register(new Skin(SkinId.CAT_CAT, "Cat Cat", "/cs230/group29se/jewelthief/Images/Entities/Player/PLAYER_SKIN_2.png"));
+        register(new Skin(SkinId.SPEED_GUY, "Speed guy", "/cs230/group29se/jewelthief/Images/Entities/Player/PLAYER_SPEEDSTER.png"));
+        register(new Skin(SkinId.CAT_CAT, "Cat Cat", "/cs230/group29se/jewelthief/Images/Entities/Player/PLAYER_TANK.png"));
     }
 
     private static void register(Skin skin) {
